f4bd353f838b569a458bd2d4f040f1d7c2d9166c
nandhini <nandhini31@gmail.com>
2017-09-06 14:46:07 +0100

    rubocop refactoring

--- spec/transcation_spec.rb
+++ spec/transcation_spec.rb
@@ -2,10 +2,10 @@ require 'transaction'
 
 describe Transaction do
 
-  let(:amount){500}
-  let(:balance){1000}
-  subject(:transaction) { described_class.new('deposit', amount,balance) }
-  subject(:transaction_withdraw) { described_class.new("withdraw", amount,balance) }
+  let(:amount){ 500 }
+  let(:balance){ 1000 }
+  subject(:transaction) { described_class.new('deposit', amount, balance) }
+  subject(:transaction_withdraw) { described_class.new('withdraw', amount, balance) }
 
   it 'returns initializes with transaction type' do
     expect(transaction.transaction_type).to eq('deposit')
